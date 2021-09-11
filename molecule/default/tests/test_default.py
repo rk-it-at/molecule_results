edd5ac78fd244da0544785549725d0407ade9db2
Rene Koch <rkoch@rk-it.at>
2021-09-11 14:51:55 +0200

    Solution for lab 9

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,10 +1,16 @@
 """Role testing files using testinfra."""
 
+def test_apache_installed(host):
+    a = host.package("httpd")
+    assert a.is_installed
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_apache_cert(host):
+    c = host.file("/etc/pki/tls/certs/localhost.crt")
+    assert c.is_file
+    assert c.user == "root"
+    assert c.group == "root"
+    assert c.mode == 0o644
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+def test_apache_port(host):
+    p = host.socket("tcp://0.0.0.0:80")
+    assert p.is_listening
